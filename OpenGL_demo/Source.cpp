041d9bbfe0c74232c444d9f18c77e5f91a31474d
Nguyen Dan <nguyendan20022002@gmail.com>
2023-06-06 20:50:12 +0700

    update

---
+++ OpenGL_demo/Source.cpp
@@ -0,0 +1,440 @@
+﻿#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"	// Sean Barrett's image loader - http://nothings.org/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <assert.h>
+
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <Matrix.h>
+
+using namespace std;
+
+typedef struct
+{
+	float xyzw[4];
+	float rgba[4];
+	float normal[4];
+	float texture[4];
+} Vertex;
+
+///    v6----- v5
+//   /|      /|
+//  v1------v0|
+//  | |     | |
+//  | |v7---|-|v4
+//  |/      |/
+//  v2------v3
+	// Mảng dữ liệu
+Vertex Vertices[] =
+{
+
+
+	// v4-v7-v6 (back - user)
+	{ {  0.5f,	-0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0,-1, 0 },{ 0.0f, 0.0f } },
+	{ { -0.5f,	-0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0,-1, 0 },{ 1.0f, 0.0f } },
+	{ { -0.5f,	 0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0,-1, 0 },{ 1.0f, 1.0f } },
+	// v6-v5-v4
+	{ { -0.5f,	 0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0,-1, 0 },{ 1.0f, 1.0f } },
+	{ {  0.5f,	 0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0,-1, 0 },{ 0.0f, 1.0f } },
+	{ {  0.5f,	-0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0,-1, 0 },{ 0.0f, 0.0f } },
+
+	// v2-v3-v0 (front - user)
+	{ { -0.5f,	-0.5f,	0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0, 1, 0 },{ 0.0f, 0.0f } },
+	{ {  0.5f,	-0.5f,	0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0, 1, 0 },{ 1.0f, 0.0f } },
+	{ {  0.5f,	 0.5f,	0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0, 1, 0 },{ 1.0f, 1.0f } },
+	// v0-v1-v2
+	{ {  0.5f,	 0.5f,	0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0, 1, 0 },{ 1.0f, 1.0f } },
+	{ { -0.5f,	 0.5f,	0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0, 1, 0 },{ 0.0f, 1.0f } },
+	{ { -0.5f,	-0.5f,	0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 0, 1, 0 },{ 0.0f, 0.0f } },
+
+	//v1-v0-v5 (top)
+	{ { -0.5f,	0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 1, 0, 0 },{ 0.0f, 0.0 } },
+	{ {  0.5f,	0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 1, 0, 0 },{ 1.0f, 0.0f } },
+	{ {  0.5f,	0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 1, 0, 0 },{ 1.0f, 1.0f } },
+	//v5-v6-v1 
+	{ {  0.5f,	0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 1, 0, 0 },{ 1.0f, 1.0f } },
+	{ { -0.5f,	0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 1, 0, 0 },{ 0.0f, 1.0f } },
+	{ { -0.5f,	0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0, 1, 0, 0 },{ 0.0f, 0.0f } },
+
+	// v7-v4-v3 (bottom)
+	{ { -0.5f,	 -0.5f,-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0,-1, 0, 0 },{ 0.0f, 0.0 } },
+	{ {  0.5f,	-0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0,-1, 0, 0 },{ 1.0f, 0.0f } },
+	{ {  0.5f,	-0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0,-1, 0, 0 },{ 1.0f, 1.0f } },
+	// v3-v2-v7
+	{ {  0.5f,	-0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0,-1, 0, 0 },{ 1.0f, 1.0f } },
+	{ { -0.5f,	-0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0,-1, 0, 0 },{ 0.0f, 1.0f } },
+	{ { -0.5f,	-0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 0,-1, 0, 0 },{ 0.0f, 0.0f } },
+
+	// v3-v4-v5 (right)
+	{ { 0.5f,	-0.5f,   0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 1, 0, 0, 0 },{ 0.0f, 0.0f } },
+	{ { 0.5f,	-0.5f,  -0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 1, 0, 0, 0 },{ 1.0f, 0.0f } },
+	{ { 0.5f,	 0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 1, 0, 0, 0 },{ 1.0f, 1.0f } },
+	// v5-v0-v3 
+	{ { 0.5f,	 0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 1, 0, 0, 0 },{ 1.0f, 1.0f } },
+	{ { 0.5f,	 0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 1, 0, 0, 0 },{ 0.0f, 1.0f } },
+	{ { 0.5f,	-0.5f,   0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ 1, 0, 0, 0 },{ 0.0f, 0.0f } },
+
+	// v7-v2-v1 (left)
+	{ { -0.5f,	-0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ -1, 0, 0, 0 },{ 0.0f, 0.0 } },
+	{ { -0.5f,	-0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ -1, 0, 0, 0 },{ 1.0f, 0.0f } },
+	{ { -0.5f,	 0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ -1, 0, 0, 0 },{ 1.0f, 1.0f } },
+	// v1-v6-v7 
+	{ { -0.5f,	 0.5f,	 0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ -1, 0, 0, 0 },{ 1.0f, 1.0f } },
+	{ { -0.5f,	 0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ -1, 0, 0, 0 },{ 0.0f, 1.0f } },
+	{ { -0.5f,	-0.5f,	-0.5f, 1.0 },{ 1.0f, 0.0f, 0.0f, 1.0f },{ -1, 0, 0, 0 },{ 0.0f, 0.0f } }
+
+};
+// ----------------------------------------
+
+const size_t BufferSize = sizeof(Vertices);
+const size_t VertexSize = sizeof(Vertices[0]);
+const size_t RgbOffset = sizeof(Vertices[0].xyzw);
+const size_t NormalOffset = sizeof(Vertices[0].xyzw) + sizeof(Vertices[1].rgba);
+const size_t TextureOffset = sizeof(Vertices[0].xyzw) + sizeof(Vertices[1].rgba) + sizeof(Vertices[1].normal);
+
+int CurrentWidth = 700,
+CurrentHeight = 700;
+
+GLuint
+VaoId,
+VboId,
+VertexShaderId,
+FragmentShaderId,
+ProgramId;
+
+mat4
+model_mat_cpp,
+view_mat_cpp,
+projection_mat_cpp;
+
+int
+model_mat_location,
+view_mat_location,
+projection_mat_location;
+
+// --------------------------------------
+bool CompileShader(GLuint shaderID) {
+	cout << "Compiling shader" << shaderID << "..." << endl;
+	glCompileShader(shaderID);
+	int result = -1;
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &result);
+	if (result != GL_TRUE) {
+		cout << "Shader " << shaderID << " compile failed" << endl;
+		int maxLength = 2048;
+		int realLength = 0;
+		char log[2048];
+		glGetShaderInfoLog(shaderID, maxLength, &realLength, log);
+		cout << log << endl;
+		return false;
+	}
+	cout << "Shader " << shaderID << " compile successful" << endl;
+	return true;
+}
+// ----------------------------------------
+
+bool LoadTexture(const char* file_name, GLuint* texture) {
+	int x, y, n;
+	int force_channels = 4;
+	stbi_set_flip_vertically_on_load(true);
+
+	// lấy vị trí của tệp ảnh đầu vào
+	// x: chiều rộng
+	// y: chiều cao
+	// : số kênh màu
+	unsigned char* image_data = stbi_load(file_name, &x, &y, &n, force_channels);
+	if (!image_data) {
+		fprintf(stderr, "ERROR: could not load %s\n", file_name);
+		return false;
+	}
+	// NPOT check
+	if ((x & (x - 1)) != 0 || (y & (y - 1)) != 0) {
+		fprintf(stderr, "WARNING: texture %s is not power-of-2 dimensions\n",
+			file_name);
+	}
+
+	// tham chiếu kết cấu
+	// 1: số lượng kết cấu muốn lưu trữ
+	// texture: lưu trữ
+	glGenTextures(1, texture);
+	glActiveTexture(GL_TEXTURE0);
+
+	// ràng buộc kết cấu
+	glBindTexture(GL_TEXTURE_2D, *texture);
+
+	// nạp dữ liệu vào texture
+	// GL_TEXTURE_2D: mục tiêu kết cấu
+	// 0: cấp độ mipmap, cấp độ cơ bản 0
+	// GL_RGBA: định dạng lưu kết cấu
+	// x, y: chiều rộng, chiều cao
+	// 0: một số công cụ kế thừa???
+	// GL_RGBA, GL_UNSIGNED_BYTE: định dạng và kiểu dữ liệu của ảnh nguồn
+	// image_data: dữ liệu hình ảnh đã nạp trước đó
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	// Texture Wrapping
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	// Texture Filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Mipmaps
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+	GLfloat max_aniso = 0.0f;
+	glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &max_aniso);
+
+	// set the maximum!
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max_aniso);
+	return true;
+}
+
+// ------------------------------------------
+string ReadShaderSourceFile(string fileName) {
+	fstream reader(fileName.c_str());
+	string line;
+	string code = "";
+	while (getline(reader, line)) {
+		code += line + "\n";
+	}
+	reader.close();
+	return code;
+}
+
+// ------------------------------------------
+void CreatVaoVbo()
+{
+	glGenVertexArrays(1, &VaoId);
+	glBindVertexArray(VaoId);
+
+	glGenBuffers(1, &VboId);
+	glBindBuffer(GL_ARRAY_BUFFER, VboId);
+	glBufferData(GL_ARRAY_BUFFER, BufferSize, Vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, VertexSize, 0); // position
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, VertexSize, (GLvoid*)RgbOffset); // color
+	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, VertexSize, (GLvoid*)NormalOffset); // normal
+	glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, VertexSize, (GLvoid*)TextureOffset); // texture
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+	glEnableVertexAttribArray(3);
+}
+// ------------------------------------------
+void CreatShaders()
+{
+	string vertexSrc = ReadShaderSourceFile("./vs.shader");
+	string fragmentSrc = ReadShaderSourceFile("./fs.shader");
+
+	const GLchar* VertexShader = vertexSrc.c_str();
+	const GLchar* FragmentShader = fragmentSrc.c_str();
+
+	VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
+	CompileShader(VertexShaderId);
+
+	FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
+	CompileShader(FragmentShaderId);
+
+	ProgramId = glCreateProgram();
+	glAttachShader(ProgramId, VertexShaderId);
+	glAttachShader(ProgramId, FragmentShaderId);
+	glLinkProgram(ProgramId);
+	glUseProgram(ProgramId);
+}
+// ------------------------------------------
+void CloseFunc()
+{
+	glUseProgram(0);
+
+	glDetachShader(ProgramId, VertexShaderId);
+	glDetachShader(ProgramId, FragmentShaderId);
+
+	glDeleteShader(FragmentShaderId);
+	glDeleteShader(VertexShaderId);
+
+	glDeleteProgram(ProgramId);
+
+	glDisableVertexAttribArray(3);
+	glDisableVertexAttribArray(2);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VboId);
+
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &VaoId);
+}
+// ------------------------------------------
+
+float
+znear = 0.1f,
+zfar = 100.0f,
+fov = 67.0f,
+aspect = (float)CurrentWidth / CurrentHeight;
+
+float	cam_speed = 0.05f,
+cam_rot_speed = 1.0f,
+cam_pos[] = { 0.0f, 0.0f, 3.0f },
+cam_x = 0.0f,
+cam_y = 0.0f,
+cam_z = 0.0f;
+
+void DisplayFunc(void)
+{
+	// nạp ảnh
+	GLuint texture;
+	LoadTexture("skulluvmap.png", &texture);
+
+	mat4 T = translate(vec3(-cam_pos[0], -cam_pos[1], -cam_pos[2]));
+
+	mat4 R = rotate_x(-cam_x);
+	//mat4 R = rotate_y(-cam_y);
+	//mat4 R = rotate_z(-cam_z);
+
+	view_mat_cpp = R * T;
+	view_mat_location = glGetUniformLocation(ProgramId, "view_mat_shader");
+	glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, view_mat_cpp.m);
+
+	projection_mat_cpp = perspective(fov, aspect, znear, zfar);
+	projection_mat_location = glGetUniformLocation(ProgramId, "projection_mat_shader");
+	glUniformMatrix4fv(projection_mat_location, 1, GL_FALSE, projection_mat_cpp.m);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glutSwapBuffers();
+}
+// ------------------------------------------
+void ReshapeFunc(int Width, int Height)
+{
+	CurrentWidth = Width;
+	CurrentHeight = Height;
+
+	glViewport(0, 0, CurrentWidth, CurrentHeight);
+}
+// ------------------------------------------
+void IdleFunc(void)
+{
+	glutPostRedisplay();
+}
+// ------------------------------------------
+void KeyboardFunc(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 27:
+		break;
+
+		// dịch theo trục x
+	case 'a':
+		cam_pos[0] -= cam_speed;
+		break;
+	case 'A':
+		cam_pos[0] -= cam_speed;
+		break;
+
+	case 'd':
+		cam_pos[0] += cam_speed;
+		break;
+	case 'D':
+		cam_pos[0] += cam_speed;
+		break;
+
+		// dịch theo trục z
+	case 'w':
+		cam_pos[2] -= cam_speed;
+		break;
+	case 'W':
+		cam_pos[2] -= cam_speed;
+		break;
+
+	case 's':
+		cam_pos[2] += cam_speed;
+		break;
+	case 'S':
+		cam_pos[2] += cam_speed;
+		break;
+
+		// quay theo trục x
+	case 'x':
+		cam_x += cam_rot_speed;
+		break;
+	case 'X':
+		cam_x -= cam_rot_speed;
+		break;
+
+		// quay theo trục y
+	case 'y':
+		cam_y += cam_rot_speed;
+		break;
+	case 'Y':
+		cam_y -= cam_rot_speed;
+		break;
+
+		// quay theo trục z
+	case 'z':
+		cam_z += cam_rot_speed;
+		break;
+	case 'Z':
+		cam_z -= cam_rot_speed;
+		break;
+	}
+}
+// ----------------------------------------
+
+	// Xử lý phím đặc biệt
+void SpecialFunc(int key, int x, int y)
+{
+	// dịch theo trục y
+	switch (key)
+	{
+	case GLUT_KEY_UP:
+		cam_pos[1] += cam_speed;
+		break;
+	case GLUT_KEY_DOWN:
+		cam_pos[1] -= cam_speed;
+		break;
+	}
+}
+
+// ------------------------------------------
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitContextVersion(4, 0);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutInitWindowSize(CurrentWidth, CurrentHeight);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutCreateWindow("Texture");
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	CreatVaoVbo();
+	CreatShaders();
+
+	glutDisplayFunc(DisplayFunc);
+	glutReshapeFunc(ReshapeFunc);
+	glutIdleFunc(IdleFunc);
+	glutKeyboardFunc(KeyboardFunc);
+	glutSpecialFunc(SpecialFunc);
+	glutCloseFunc(CloseFunc);
+
+	glutMainLoop();
+	return 0;
+}
---
+++ OpenGL_demo/vs.shader
@@ -0,0 +1,18 @@
+#version 460
+
+layout(location = 0) in vec4 vertex_position;
+layout(location = 1) in vec4 vertex_color;
+layout(location = 2) in vec4 vertex_normal;
+layout(location = 3) in vec2 texture_position;
+
+uniform mat4
+model_mat_shader,
+view_mat_shader,
+projection_mat_shader;
+
+out vec2 texture_coordinates;
+
+void main() {
+	texture_coordinates = texture_position;
+	gl_Position = projection_mat_shader * view_mat_shader * vertex_position;
+}
